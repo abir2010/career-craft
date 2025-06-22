0ecb01ff9d37113a2c5ff5643494ac9ebcc1495b
M.K. Abir <kaiserabir222@gmail.com>
2025-06-22 22:42:34 +0000

    final commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import { AppLayout } from "@/components/AppLayout";
 import { Toaster } from "@/components/ui/toaster";
+import { Analytics } from "@vercel/analytics/next"
 
 export const metadata: Metadata = {
   title: "CareerCraft AI",
@@ -22,7 +23,7 @@ export default function RootLayout({
         <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700&display=swap" rel="stylesheet" />
       </head>
       <body className="font-body antialiased">
-        <AppLayout>{children}</AppLayout>
+        <AppLayout>{children}<Analytics/></AppLayout>
         <Toaster />
       </body>
     </html>
